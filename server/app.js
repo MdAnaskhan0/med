8a704bd4834e06d3ebde8ee4a05cfa218b52d049
Md. Anas Khan <108482138+MdAnaskhan0@users.noreply.github.com>
2025-06-28 13:38:27 +0600

    Update app.js

--- server/app.js
+++ server/app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
   if (req.method === 'OPTIONS') {
