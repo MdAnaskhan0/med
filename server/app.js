4ee672caef090493b4a4dc6860f56b9aad5acd23
MdAnaskhan0 <mdanaskhan.201902037@gmail.com>
2025-08-04 15:22:46 +0600

    origin update.

--- server/app.js
+++ server/app.js
@@ -27,6 +27,8 @@ const app = express();
 
 // Configure allowed origins
 const allowedOrigins = [
+  'http://192.168.111.140:5173',
+  'http://192.168.111.140:5174',
   'https://med-movement.vercel.app', // Your frontend
   'https://med-admin-khaki.vercel.app',
   'https://med-7bj4.onrender.com'
